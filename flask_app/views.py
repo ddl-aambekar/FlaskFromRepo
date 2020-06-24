5ab513c3a6ff601e73a7aeb0f605c8c78374c542
ddl-salme <62372947+ddl-salme@users.noreply.github.com>
2020-06-24 15:01:07 -0500

    Add files via upload

---
+++ flask_app/views.py
@@ -0,0 +1,12 @@
+from flask_app import app
+import flask
+from flask import request, redirect, url_for
+
+@app.route('/')
+def index():
+    return flask.render_template("index.html")
+    
+    
+@app.route('/another_page')
+def another_page():
+    return "Hello New York!"
